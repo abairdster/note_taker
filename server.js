08cf8ca1d6413eb046a982d4fa103ae3fa518ae2
Andrew Baird <bairdfix@gmail.com>
2022-04-28 22:44:24 -0500

    MVP

--- server.js
+++ server.js
@@ -20,14 +20,18 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.get('/notes', (req, res) => {
+//app.get('/notes', (req, res) => {
+//    res.sendFile(path.join(__dirname, './public/notes.html'))
+//})
+
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
 app.use(express.static("public"))
 const allRoutes = require("./server.js");
 const { all } = require('express/lib/application');
-app.use("allRoutes");
+//app.use("allRoutes");
 
 app.listen(PORT,() =>{
     console.log("App listening on port:" + PORT);
