a67a2d0f8be184f6557b0d53c9bbf400242cb22b
Andrew Baird <bairdfix@gmail.com>
2022-04-29 12:23:38 -0500

    heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let routes = require('./routes/routes.js')
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,12 +20,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-//app.get('/notes', (req, res) => {
-//    res.sendFile(path.join(__dirname, './public/notes.html'))
-//})
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
+   res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
 app.use(express.static("public"))
